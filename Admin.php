e79bb4c05d770c885d05035c508ed0ac20315442
Hady Safa <hadyabdallahsafa@gmail.com>
2025-04-04 11:22:28 +0300

    Added the dashboards

---
+++ Admin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="./Style/dashboard.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <section class="links">
+            <a href="" class="button">
+                <img class="icon" src="./Icons/settings.svg" alt="icon" />
+                <h4>Users Management</h4>
+            </a>
+            <a href="" class="button">
+                <img class="icon" src="./Icons/list.svg" alt="icon" />
+                <h4>Products Management</h4>
+            </a>
+            <a href="" class="button">
+                <img class="icon" src="./Icons/clipboard.svg" alt="icon" />
+                <h4>Orders Management</h4>
+            </a>
+            <a href="" class="button">
+                <img class="icon" src="./Icons/clipboard.svg" alt="icon" />
+                <h4>Stock Management</h4>
+            </a>
+        </section>
+    </div>
+</body>
+
+</html>
---
+++ User.php
@@ -0,0 +1,58 @@
+<!--This is done by hady-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+    <link rel="stylesheet" href="./Style/dashboard.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <section class="info">
+            <h2 class="name">Hady Safa</h2>
+            <div class="balance     ">
+                <div>
+                    <img class="icon" src="./Icons/wallet.svg" alt="icon" />
+                    <h4>Balance</h4>
+                </div>
+                <div>
+                    <p>20$</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="links">
+            <a href="" class="button">
+                <img class="icon" src="./Icons/settings.svg" alt="icon" />
+                <h4>Profile</h4>
+            </a>
+            <a href="" class="button">
+                <img class="icon" src="./Icons/list.svg" alt="icon" />
+                <h4>Orders</h4>
+            </a>
+            <a href="" class="button">
+                <img class="icon" src="./Icons/clipboard.svg" alt="icon" />
+                <h4>Purchase List</h4>
+            </a>
+        </section>
+
+        <section class="signout">
+            <form action="">
+                <button class="button" type="submit">
+                    <img class="icon" src="./Icons/signout.svg" alt="icon" />
+                    <h4>Sign Out</h4>
+                </button>
+            </form>
+        </section>
+
+    </div>
+
+</body>
+
+</html>
